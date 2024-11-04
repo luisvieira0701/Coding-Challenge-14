2fbc4415422ef9144978d57af03270f96373e9ff
LuisVieira0701 <luisvieira@usf.edu>
2024-11-03 21:50:05 -0500

    Task3 - Dynamically display tickets on the page.

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 // Task 2 Fetch Tickets Using Async/Await and Handle Errors
 async function fetchTicketData() {
-    const ticketDiv = document.getElementById("ticket-display");
     const errorMessage = document.getElementById("error-message")
 
 
@@ -24,9 +23,30 @@ async function fetchTicketData() {
     }  
 }
 
+//Task 3- Display Tickets Dynamically on the Page
 
+tickets.forEach(ticket=>{
+    const ticketDiv = document.getElementById("ticket-display");
+    ticketDiv.classList.add("ticket")
 
+    const ticketId = document.createElement("h3")
+    ticketId.textContent = `Ticket ID: ${ticket.id}`;
 
+    const customerName = document.createElement("p")
+    customerName.textContent = `Customer Name: user ${ticket.userID}`
 
+    const issueDescription = document.createElement("p")
+    issueDescription.textContent = `Error Description: ${ticket.title}`
+
+    const details = document.createElement("p")
+    details.textContent = `Ticket Details: ${ticket.body}`
+
+    ticketDiv.appendChild(ticketId)
+    ticketDiv.appendChild(customerName)
+    ticketDiv.appendChild(issueDescription)
+    ticketDiv.appendChild(details)
+        
+
+})
 
 fetchTicketData()
